4131f53c0c2b2a8a290382635736cecc07061c3d
vipin091 <58137324+vipin091@users.noreply.github.com>
2022-11-21 19:26:08 +0530

    Add files via upload

--- routes/product.js
+++ routes/product.js
@@ -1,1 +1,12 @@
+const router = require("express").Router();
+const productController = require('../controllers/productController');
+const middleware = require("../controllers/MiddlewareController");
 
+router.post("/", productController.product_create);
+router.get("/", productController.product_all);
+router.get("/:productId", middleware, productController.product_details);
+router.put("/:productId", middleware, productController.product_update);
+router.delete("/:productId", middleware, productController.product_delete);
+//router.post("/", productController.job_scheduler);
+
+module.exports = router;
